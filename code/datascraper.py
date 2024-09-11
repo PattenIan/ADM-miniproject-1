cdb281384ba8135185e49bac97bd2c986b61d3a8
Patrick <paknu22@student.sdu.dk>
2024-09-12 00:28:05 +0200

    I think it works, I keep getting refused access by the server

--- code/datascraper.py
+++ code/datascraper.py
@@ -1,26 +1,28 @@
 import requests
 import dill
 import re
+import time
 from bs4 import BeautifulSoup
 from datetime import datetime
 
 links = "https://web.archive.org/web/20150918040703/http://www.newyorksocialdiary.com/party-pictures?page=1"
 
-def get_page_links():
-    page = requests.get("https://web.archive.org/web/20150918040703/http://www.newyorksocialdiary.com/party-pictures?page=1")
+def get_page_links(page_link):
+    page = requests.get(page_link)
     soup = BeautifulSoup(page.text, "lxml")
     links = soup.find_all(class_="views-row")
     #print(links, "   ", len(links))
-    print("URL 1: ", get_link_date(links[0]))
-    print("URL 2: ", get_link_date(links[1]))
-    print("URL 3: ", get_link_date(links[2]))
-    print("URL 4: ", get_link_date(links[3]))
-    print("URL 5: ", get_link_date(links[4]))
+    #print("URL 1: ", get_link_date(links[0]))
+    #print("URL 2: ", get_link_date(links[1]))
+    #print("URL 3: ", get_link_date(links[2]))
+    #print("URL 4: ", get_link_date(links[3]))
+    #print("URL 5: ", get_link_date(links[4]))
     #return links
 
-    links_date = [get_link_date(link) for link in links]
+    #links_date = [get_link_date(link) for link in links]
 
-    print(filter_by_date(links=links_date))
+    #print(filter_by_date(links=links_date))
+    return links
     
 def get_link_date(link):
     titleField = link.find(class_="field-content")
@@ -47,4 +49,27 @@ def filter_by_date(links, cutoff=datetime(2014, 12, 1)):
             filtered.append(link)
     return filtered
 
+
+def gather_all_links():
+    page_links = []
+    all_links = []
+    filtered_links = []
+    for page in range (1,25):
+        page_link = "https://web.archive.org/web/20150918040703/http://www.newyorksocialdiary.com/party-pictures?page=" + str(page)
+        page_links.append(page_link)
+    
+    
+    for pl in range(0, len(page_link)):
+        sleep_timer = 1
+        all_links.append(get_page_links(page_links[pl]))
+        if pl >= 15:
+            sleep_timer = 10
+        time.sleep(sleep_timer)
+        
+        print("link done")
+    
+    for link in all_links:
+        filtered_links.append(filter_by_date(link))
+    print(filtered_links) 
+    
     
--- code/main.py
+++ code/main.py
@@ -2,7 +2,8 @@ import datascraper
 
 def main():
     print("Hello, world!")
-    datascraper.get_page_links()
+    #datascraper.get_page_links()
+    datascraper.gather_all_links()
 
 if __name__ == "__main__":
     main()
