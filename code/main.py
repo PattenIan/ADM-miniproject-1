92faaf89c357cadcb2431089ae035690a1363d52
Patrick <paknu22@student.sdu.dk>
2024-09-25 23:30:30 +0200

    Fixed couple names where they are seperated by with

--- code/main.py
+++ code/main.py
@@ -4,7 +4,7 @@ import regex_patterns
 
 def main():
     #regex_patterns.process_shared_last_names("Removed_empty_brackets.txt", "Short_caption_couple_names.txt")
-    regex_patterns.process_shared_last_names("Removed_photographers.txt", "All_couples_together.txt")
+    regex_patterns.process_shared_last_names("All_couples_together.txt", "All_couples_together2.txt")
 
 if __name__ == "__main__":
     main()
